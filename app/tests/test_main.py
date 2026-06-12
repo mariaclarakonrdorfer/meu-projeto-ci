3ba2459ea4982d1c436bd90118f52e8486471e2d
mariaclarakonrdorfer <164265100+mariaclarakonrdorfer@users.noreply.github.com>
2026-06-12 10:45:15 -0300

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,9 +1,10 @@
-import pytest
 from app.main import somar, conectar_api
+
 
 def test_somar_correto():
     assert somar(2, 3) == 5
 
+
 def test_conectar_api_com_sucesso():
-    # O teste passará se o segredo for injetado corretamente pelo GitHub Actions
+    # O teste passa se o segredo for injetado corretamente
     assert conectar_api() == "Conexão Estabelecida com Sucesso!"
