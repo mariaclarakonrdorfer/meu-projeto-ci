70b00c0f13fae31b21b074411cf3c0068395f44a
mariaclarakonrdorfer <164265100+mariaclarakonrdorfer@users.noreply.github.com>
2026-06-12 09:56:06 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import os
+
+def somar(a, b):
+    return a + b
+
+def conectar_api():
+    # Simulando o consumo de um Secret injetado em memória
+    token = os.environ.get("MINHA_API_SECRET")
+    if not token:
+        raise ValueError("ERRO DE SEGURANÇA: Token não encontrado!")
+    return "Conexão Estabelecida com Sucesso!"
